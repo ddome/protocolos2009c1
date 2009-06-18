4020e423fd59633a39a28a084b7b3ec0ba3abb46
bombax <bombax@0d275870-3859-11de-9d05-31133e6853b1>
2009-06-18 21:19:40 +0000

    Nueva implementacion de la encriptacion

--- Common/cypher.h
+++ Common/cypher.h
@@ -1,52 +1,37 @@
-#ifndef _CYPHER_
-#define _CYPHER_
+#ifndef _CYPHER_H
+#define _CYPHER_H
 
-#include "./des/include/bit.h"
-#include "./des/include/encrypt.h"
-#include<string.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include "./des/include/bit.h" 
+#include "./des/include/encrypt.h" 
 
-
-/*ATENCION: Maxima longitud del password. De ser excedida se truncara el string.
- *Es decir que el password "12345678" y "12345678a" son lo mismo.
-*/
 #define CYPHER_SIZE 8
+#define CYPHER_ERR -1
 
+char * Cypher(char * original,int size,char * password);
 
-
-/*Retorno de las funciones si no se produjo ningun error.*/
-#define OK 0
-/*Retorno de las funciones si se produjo algun error.*/
-#define ERROR_CYPHER -1
-
-
-
-/* Encripta un secuencia de bytes dados.
- *
- *	resp:	Almacena los datos ya encriptados en *resp. La funcion se
- *		se encarga de alocar el espacio suficiente. El usuario es
- *		responsable de liberar dicho espacio.
- *	msg:	Los datos a encriptar.
- *	size:	El tamaño de los datos a encriptar.
- *	passwd:	Password para encriptar los datos.
-*/
-int Cypher(char ** resp,const char * msg,size_t size,const char * passwd);
-
-
-
-/* Desencripta un secuencia de bytes encriptados previamente con la funcion
- *Cypher.
- *
- *	resp:	Almacena los datos desencriptados en *resp. La funcion se
- *		se encarga de alocar el espacio suficiente. El usuario es
- *		responsable de liberar dicho espacio.
- *	msg:	Los datos a desencriptar.
- *	size:	El tamaño de los datos a desencriptar. El tamaño original.
- *		No el tamaño de los datos ya encriptados.
- *	passwd:	Password para desencriptar los datos. Debe coincidir con el
- *		que se uso para encriptarlos.
-*/
-int Decypher(char ** resp,const char * msg,size_t size,const char * passwd);
+char * Decypher(char * original,int size,char * password);
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
